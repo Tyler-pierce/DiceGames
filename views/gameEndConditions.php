663459251bcf8ffed2965d0fe1629f9e45b59653
Tyler Pierce <tyler.pierce@gmail.com>
2011-08-29 18:26:35 -0700

    cleaned up the client to show all needed output, added auto-crediting so funds always available to trial

---
+++ views/gameEndConditions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *  The output for game end conditions
+ */
+
+?>
+
+<div class="dc_game_conditions">
+    <div class="dc_game_conditions_inside dc_text3">
+        Race to <span id="dc_wins_to_end">0</span>
+    </div>
+</div>
